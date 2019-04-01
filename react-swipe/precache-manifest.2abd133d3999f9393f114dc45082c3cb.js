bdd34798622876abff48350842748b162997657e
teropaj <teropaj@gmail.com>
2019-04-01 20:59:49 +0300

    again

---
+++ react-swipe/precache-manifest.2abd133d3999f9393f114dc45082c3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60adeb19301395546700",
+    "url": "/static/css/main.f9d42346.chunk.css"
+  },
+  {
+    "revision": "60adeb19301395546700",
+    "url": "/static/js/main.8a3744ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "414a025ff0c261936786",
+    "url": "/static/js/2.41b3ae7e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c209f559d77cfcfa434196d6f201736",
+    "url": "/index.html"
+  }
+];
