08568cb5c06a6640e17b88f061bec141fdcabb3d
teropaj <teropaj@gmail.com>
2019-04-12 07:55:17 +0300

    teacher view

---
+++ priima-t-view/precache-manifest.6894de2dcea0cb5511504cfe21e1ae32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bebe7b715ee0e78946a1",
+    "url": "/static/css/main.148957f0.chunk.css"
+  },
+  {
+    "revision": "bebe7b715ee0e78946a1",
+    "url": "/static/js/main.bf2f5de2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "834f7c770a78307e88c8b47cddf0c129",
+    "url": "/index.html"
+  }
+];
