876b5fccfdc0b7a8ba3db95c59a07ac394e003c5
teropaj <teropaj@gmail.com>
2020-02-28 10:57:16 +0200

    initial

---
+++ preexam/logig.js
@@ -0,0 +1,33 @@
+let wordItem=0
+
+//document.getElementById('words').innerHTML=localStorage.getItem(Object.keys(localStorage)[wordItem])
+document.getElementById('words').innerHTML=Object.keys(localStorage)[wordItem] 
+function addWord (word,value) { localStorage.setItem(word,value)}
+
+document.getElementById('addWord').addEventListener("submit", logSubmit)
+
+document.getElementById('nextWord').addEventListener("click", nextWord)
+
+//show word
+document.getElementById('showWord').addEventListener("click", showWord)
+function logSubmit (event)
+    {    
+        console.log(event)
+        debugger
+        localStorage.setItem(event.target[0].value, event.target[1].value) 
+        event.preventDefault();
+        //console.log(localStorage.event.target[0])
+    }
+
+function nextWord() {
+    (wordItem<localStorage.length-1) ? wordItem++ : wordItem=0
+    document.getElementById('words').innerHTML=Object.keys(localStorage)[wordItem] 
+}
+
+function showWord() {
+    console.log('was here')
+    //debugger
+    document.getElementById('value').innerHTML=localStorage.getItem(Object.keys(localStorage)[wordItem])
+    setTimeout(()=> document.getElementById('value').innerHTML="",
+    2000) 
+}
