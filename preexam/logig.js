9d4f7e103a64a4d628cd35fb6afa919d14935558
teropaj <teropaj@gmail.com>
2020-03-09 23:54:56 +0200

    modal added

--- preexam/logig.js
+++ preexam/logig.js
@@ -7,7 +7,9 @@ function addWord (word,value) { localStorage.setItem(word,value)}
 
 document.getElementById('addWord').addEventListener("submit", logSubmit)
 
-document.getElementById('nextWord').addEventListener("click", nextWord)
+nextWordButton=document.getElementById('nextWord')
+nextWordButton.addEventListener("click", nextWord)
+idWords=document.getElementById('words')
 
 //show word
 document.getElementById('showWord').addEventListener("click", showWord)
@@ -40,14 +42,19 @@ function logSubmit (event)
 function nextWord() {
     (wordItem<localStorage.length-1) ? wordItem++ : wordItem=0
     document.getElementById('words').innerHTML=Object.keys(localStorage)[wordItem] 
+     
+    idWords.classList.add('animate-in');
+    setInterval(()=>idWords.classList.remove('animate-in')
+    , 1000)
 }
 
 function showWord() {
     console.log('was here')
     //debugger
-    document.getElementById('value').innerHTML=localStorage.getItem(Object.keys(localStorage)[wordItem])
-    setTimeout(()=> document.getElementById('value').innerHTML="",
+    //document.getElementById('value').innerHTML=localStorage.getItem(Object.keys(localStorage)[wordItem])
+    document.getElementById('words').innerHTML=localStorage.getItem(Object.keys(localStorage)[wordItem],
     2000) 
+    setTimeout(()=>document.getElementById('words').innerHTML=Object.keys(localStorage)[wordItem], 2000)
 }
 
 function deleteWord() {
@@ -61,4 +68,55 @@ function deleteWord() {
     if(deleteCounter=== 0) {localStorage.removeItem(Object.keys(localStorage)[wordItem])
     document.getElementById('delete').innerHTML="delete"}
 }
+function animate() {
+    title.classList.remove('animate-in');
+    for (var i = 0; i < courseFeatureElements.length; i++) {
+      courseFeatureElements[i].classList.remove('animate-in');
+    }
+    button.classList.remove('animate-in');
+  
+    setTimeout(function () {
+      title.classList.add('animate-in');
+    }, 1000);
+  
+    setTimeout(function () {
+      courseFeatureElements[0].classList.add('animate-in');
+    }, 3000);
+  
+    setTimeout(function () {
+      courseFeatureElements[1].classList.add('animate-in');
+    }, 4500);
+  
+    setTimeout(function () {
+      courseFeatureElements[2].classList.add('animate-in');
+    }, 6000);
+  
+    setTimeout(function () {
+      courseFeatureElements[3].classList.add('animate-in');
+    }, 7500);
+  
+    setTimeout(function () {
+      courseFeatureElements[4].classList.add('animate-in');
+    }, 9000);
+  
+    setTimeout(function () {
+      courseFeatureElements[5].classList.add('animate-in');
+    }, 10500);
+  
+    setTimeout(function () {
+      courseFeatureElements[6].classList.add('animate-in');
+    }, 12000);
+  
+    setTimeout(function () {
+      button.classList.add('animate-in');
+    }, 13500);
+  }
+  function addWords () {console.log('was here')
+  document.querySelector('.hidden').classList.remove('hidden')
+  document.querySelector('#menu').classList.add('hidden')
+}
+  function closeAdd () {
+    document.querySelector('#add').classList.add('hidden')
+    document.querySelector('#menu').classList.remove('hidden')
+  }
 
