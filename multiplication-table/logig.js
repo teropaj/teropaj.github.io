09597e1a6af3dd05b71db5e0ca8e16fe90c39212
teropaj <teropaj@gmail.com>
2020-03-25 13:47:58 +0200

    kertolasku

---
+++ multiplication-table/logig.js
@@ -0,0 +1,25 @@
+function value () {
+    return Math.floor(Math.random()*10+1)
+}
+ 
+
+let tulo,b,c
+ 
+
+ 
+
+function next () {
+    b=value();c=value()
+     
+    a=b+`*`+ c  
+    document.getElementById('kys').innerHTML=a
+
+}
+function vastaus () {
+    tulo=b*c+""
+    document.getElementById('kys').innerHTML=b+`*`+ c+ ' = '+tulo
+}
+next()
+document.getElementById('kys').innerHTML=a
+
+ 
